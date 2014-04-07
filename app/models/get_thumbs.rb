652bbbb8f0d5c489e10573a7ea6f4edf214d06a6
Magdalena Lipinska <mag.lipinska@gmail.com>
2014-04-07 11:20:38 -0400

    writing tests for thumb methid for conference class

--- app/models/get_thumbs.rb
+++ app/models/get_thumbs.rb
@@ -1,18 +1,19 @@
 class GetThumbs
 
   THUMBS = {
-    marionette: :"http://kroltech.com/wp-content/uploads/2013/08/marionette1.jpg",
-    backbone: :"https://scontent-b-iad.xx.fbcdn.net/hphotos-prn1/t1/5543_446948895373302_1141770560_n.jpg",
-    rails: :"https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQUyE5VORKuBi2pcTVcU0XPDwK5uIDlau5Ajz6vXd_rzICeHMcs",
-    ruby:  :"http://www.calstatela.edu/sites/default/files/centers/hipic/classes/images/Ruby-logo-2008.png",
-    javascript: :"/assets/js.jpg"
+    marionette: "http://kroltech.com/wp-content/uploads/2013/08/marionette1.jpg",
+    backbone: "https://scontent-b-iad.xx.fbcdn.net/hphotos-prn1/t1/5543_446948895373302_1141770560_n.jpg",
+    rails: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQUyE5VORKuBi2pcTVcU0XPDwK5uIDlau5Ajz6vXd_rzICeHMcs",
+    ruby:  "http://www.calstatela.edu/sites/default/files/centers/hipic/classes/images/Ruby-logo-2008.png",
+    javascript: "/assets/js.jpg",
+    default: "http://blogs.microsoft.co.il/blogs/gilf/CSS3Logo_thumb_57496FDA.jpg"
   }
 
   def self.get(tags)
     THUMBS.keys.each do |tag|
-      return THUMBS[tag].to_s if tags.downcase.include?(tag.to_s)
+      return THUMBS[tag] if tags.downcase.include?(tag.to_s)
     end
-    return
+    THUMBS[:default]
   end
 
 end
--- spec/models/conference_spec.rb
+++ spec/models/conference_spec.rb
@@ -34,5 +34,25 @@ describe Conference do
       @c.save
       expect(@c).to be_valid
     end
+
+
+    it "finds a thumbnail for a given tag" do
+      @c.tags = "javascript"
+      @c.save
+      expect(@c.thumb).to eq("/assets/js.jpg")
+    end
+
+    it "gives default tumb if tags unknown" do
+      @c.tags = "dsdada"
+      @c.save
+      expect(@c.thumb).to eq("http://blogs.microsoft.co.il/blogs/gilf/CSS3Logo_thumb_57496FDA.jpg")
+    end
+
+    it "returns first matching tag value" do
+      @c.tags = "backbone, rails, ruby"
+      @c.save
+      expect(@c.thumb).to eq("https://scontent-b-iad.xx.fbcdn.net/hphotos-prn1/t1/5543_446948895373302_1141770560_n.jpg")
+    end
+
   end
 end
