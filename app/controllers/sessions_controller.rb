a99440ab0f38e768c704adbeb71e0048da412bb4
Magdalena Lipinska <mag.lipinska@gmail.com>
2014-04-07 09:53:34 -0400

    deleting notice

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
-    redirect_to root_url, :notice => "Signed in!"
+    redirect_to root_url
   end
 
   def destroy
