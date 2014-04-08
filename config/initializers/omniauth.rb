6d7d2707e814fc16af2a5b263a5a35fe982f758f
Magdalena Lipinska <mag.lipinska@gmail.com>
2014-04-07 22:49:22 -0400

    facebook is responding

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,7 +6,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   end
   provider :developer unless Rails.env.production?
   provider :facebook, '1401151870160328', 'd35cede0b6c6561ef88dd809be401c5a' if Rails.env.development?
-           :scope => 'email', :display => 'popup'
   provider :facebook, '1401151870160328', 'd35cede0b6c6561ef88dd809be401c5a' if Rails.env.production?
   provider :twitter, 'If6w3RG4lsiZaYxCare3Iw', 'KQ79jk6LS2AdWcBvUnmWwRomjOiQ4VxQmwC1lQDy7A'
   # provider :github, 'a51b4893fbf0e4334f33', '16fd6cc9254cf1c14107611df2698d03c9ae4d95' if Rails.env.development?
