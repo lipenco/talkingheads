4da8aea66f6e118870c7c649589c63646416d0d1
Magdalena Lipinska <mag.lipinska@gmail.com>
2014-04-14 09:32:34 -0400

    fixing favorites_num bug

--- app/models/favourite.rb
+++ app/models/favourite.rb
@@ -2,9 +2,6 @@ class Favourite < ActiveRecord::Base
   belongs_to :user
   belongs_to :talk
 
-  validates_uniqueness_of :talk_id
-
-
   def favourite_for(user, id)
      return false unless user
   end
