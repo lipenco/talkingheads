490330c833f27a4f96326d2cb678c14760e6fc29
Magdalena Lipinska <mag.lipinska@gmail.com>
2014-04-21 20:39:45 -0400

    adding comment model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140416160118) do
+ActiveRecord::Schema.define(version: 20140422003832) do
+
+  create_table "comments", force: true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.integer  "talk_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["talk_id"], name: "index_comments_on_talk_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "conferences", force: true do |t|
     t.string   "name"
